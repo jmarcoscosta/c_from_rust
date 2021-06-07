dc16932aed44c5ac3480783920b7f56530b370db
Joao Marcos Costa <jmcosta944@gmail.com>
2021-06-06 21:57:54 -0300

    Add binding.rs generation to build.rs
    binding.rs is now automatically generated, and thus it will be removed
    from src directory in a follow-up commit.
    
    Signed-off-by: Joao Marcos Costa <jmcosta944@gmail.com>

--- build.rs
+++ build.rs
@@ -1,6 +1,21 @@
 extern crate cc;
+extern crate bindgen;
+use std::env;
+use std::path::PathBuf;
 
 fn main() {
+    // Automatically generate the binding.rs file
+    let bindings = bindgen::Builder::default()
+        .header("src/fibonacci.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("binding.rs"))
+        .expect("Couldn't write bindings!");
+
     cc::Build::new()
         .file("src/fibonacci.c")
         .compile("libfibonacci.a");
